aa3da948888bc419af2b349945e2a1091081da95
Maria Luìsa <malubritoc@MacBook-Air-de-Maria-2.local>
2026-04-24 12:25:14 -0300

    chores: lint fixes and github workflows creation

--- prototipo-frontend/src/app/pages/products-list/product-card/product-card.spec.ts
+++ prototipo-frontend/src/app/pages/products-list/product-card/product-card.spec.ts
@@ -1,18 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProductCardComponent } from './product-card';
 
-import { ProductCard } from './product-card';
-
-describe('ProductCard', () => {
-  let component: ProductCard;
-  let fixture: ComponentFixture<ProductCard>;
+describe('ProductCardComponent', () => {
+  let component: ProductCardComponent;
+  let fixture: ComponentFixture<ProductCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductCard]
-    })
-    .compileComponents();
+      imports: [ProductCardComponent]
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(ProductCard);
+    fixture = TestBed.createComponent(ProductCardComponent);
+    fixture.componentRef.setInput('product', {
+      id: 1,
+      title: 'Produto Teste',
+      price: 10,
+      image: 'test.png',
+      stock: 1,
+    });
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
