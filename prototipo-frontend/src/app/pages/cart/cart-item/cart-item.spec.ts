99dd0777714a1be047f6990ea3a808e548944933
Gabriel Rodrigues <gab_gro@hotmail.com>
2026-06-28 15:26:23 -0300

    fix 3

--- prototipo-frontend/src/app/pages/cart/cart-item/cart-item.spec.ts
+++ prototipo-frontend/src/app/pages/cart/cart-item/cart-item.spec.ts
@@ -13,6 +13,13 @@ describe('CartItem', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(CartItemComponent);
+    fixture.componentRef.setInput('product', {
+      id:4,
+      title: 'Coca Cola 350 ml',
+      price: 5.99,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ36sMgVakFA-E3GEl5x36h76y_F4z9QL1BXC8hAe-1nlFMhN-S5uzvL8Y&s=10',
+      stock: 7,
+    });
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
